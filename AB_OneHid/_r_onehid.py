12cb959c2f70be4974a4c5365852b19edb691c35
Polina Oleneva <iKosh@Polinas-MacBook-Pro.local>
2019-06-06 10:35:39 +0300

    tuning hidden layer size

--- AB_OneHid/_r_onehid.py
+++ AB_OneHid/_r_onehid.py
@@ -21,21 +21,38 @@ print('The shape of Y is: ' + str(shape_Y))
 print('I have m = %d training examples!' % (m))
 
 # Simple logistic regression
-print("\033[1m" + "SKLearn logistic regression:" + "\033[0m")
+print('\n' + "\033[1m" + "SKLearn logistic regression:" + "\033[0m")
 clf = sklearn.linear_model.LogisticRegressionCV();
 clf.fit(X.T, Y.T);
 # Plot the decision boundary for logistic regression
 plot_decision_boundary(lambda x: clf.predict(x), X, Y.ravel())
 plt.title("Logistic Regression")
-
 # Print accuracy
 LR_predictions = clf.predict(X.T)
-print ('Accuracy of logistic regression: %d ' % float((np.dot(Y,LR_predictions) + np.dot(1-Y,1-LR_predictions))/float(Y.size)*100) +
+print('Accuracy of logistic regression: %d ' % float((np.dot(Y,LR_predictions) + np.dot(1-Y,1-LR_predictions))/float(Y.size)*100) +
        '% ' + "(percentage of correctly labelled datapoints)")
 
 # Build a model with a n_h-dimensional hidden layer
+print('\n' + "\033[1m" + "One Hidden Layer Neural Network:" + "\033[0m")
 parameters = onehid(X, Y, n_h=4, num_iterations=10000, print_cost=True)
 
 # Plot the decision boundary
 plot_decision_boundary(lambda x: predict(parameters, x.T), X, Y)
 plt.title("Decision Boundary for hidden layer size " + str(4))
+
+# Print accuracy
+predictions = predict(parameters, X)
+print('Accuracy: %d' % float((np.dot(Y, predictions.T) + np.dot(1-Y, 1-predictions.T))/float(Y.size)*100) + '%')
+
+# Tuning hidden layer size
+print('\n' + "\033[1m" + "Tuning hidden layer size:" + "\033[0m")
+#plt.figure(figsize=(16, 32))
+hidden_layer_sizes = [1, 2, 3, 4, 5, 20, 50]
+for i, n_h in enumerate(hidden_layer_sizes):
+    #plt.subplot(5, 2, i+1)
+    plt.title('Hidden Layer of size %d' % n_h)
+    parameters = onehid(X, Y, n_h, num_iterations=5000)
+    plot_decision_boundary(lambda x: predict(parameters, x.T), X, Y)
+    predictions = predict(parameters, X)
+    accuracy = float((np.dot(Y, predictions.T) + np.dot(1-Y, 1-predictions.T))/float(Y.size)*100)
+    print("Accuracy for {} hidden units: {} %".format(n_h, accuracy))
